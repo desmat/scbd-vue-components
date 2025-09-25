8bcbadf59311da31c1a0dd96bea0295d2f4d9e90
Mathieu Desjarlais <github@desmat.ca>
2025-09-25 14:18:19 -0400

    add date, datetime, date range, datetime range input components

--- lib/src/App.vue
+++ lib/src/App.vue
@@ -1,30 +1,71 @@
 <!-- for testing locally -->
-<script setup lang="ts">
-import { computed, ref } from 'vue'
+<script
+  setup
+  lang="ts"
+>
+import { ref } from 'vue'
 import MyButton from './components/my-button.vue'
-import MultiLanguageInput from './components/multi-language-input.vue';
+import MultiLanguageInput from './components/multi-language-input.vue'
+import DateInput from './components/date-input.vue'
+import DateRangeInput from './components/date-range-input.vue'
+import DatetimeInput from './components/datetime-input.vue'
+import DatetimeRangeInput from './components/datetime-range-input.vue'
 
 const langValues = ref({
   en: 'English value',
   fr: 'La valeure française'
 })
+
+const date = ref('2000-01-01');
+const datetime = ref(new Date().toISOString());
+const timezone = "America/Montreal";
 </script>
 
 <template>
-  <div style="width: fit-content; padding: 1rem; display: flex; flex-direction: column; align-items: center; margin: auto">
-    <h2>this is the lib</h2>
-    <div class="d-flex flex-column gap-2">
-      <MyButton msg="Allo" />
-      <MyButton msg="Hello" />
-      <MultiLanguageInput label="Multi Language Input" v-model="langValues"/>
-      <MultiLanguageInput
-        label="Multi Language Input (valid)"
-        :valid="{ en: true, fr: true }"
-      />
-      <MultiLanguageInput
-        label="Multi Language Input (invalid)"
-        :valid="{ en: false }"
-      />
-    </div>
+  <div
+    style="width: fit-content; padding: 1rem; display: flex; flex-direction: column; gap: 1rem; align-items: center; margin: auto"
+  >
+    <h1>this is the lib</h1>
+
+    <h2>MyButton</h2>
+    <MyButton msg="Allo" />
+    <MyButton msg="Hello" />
+
+    <h2>Multi Language Input</h2>
+    <MultiLanguageInput
+      label="Multi Language Input"
+      v-model="langValues"
+    />
+    <MultiLanguageInput
+      label="Multi Language Input (valid)"
+      :valid="{ en: true, fr: true }"
+    />
+    <MultiLanguageInput
+      label="Multi Language Input (invalid)"
+      :valid="{ en: false }"
+    />
+
+    <h2>Date Inputs</h2>
+    <DateInput
+      label="Date"
+      v-model="date"
+    ></DateInput>
+    <DateRangeInput
+      label="Date"
+      v-model:start-date="date"
+      v-model:end-date="date"
+    />
+    <DatetimeInput
+      label="Datetime"
+      v-model="datetime"
+      :timezone="timezone"
+    />
+    <DatetimeRangeInput
+      label="Datetime"
+      v-model:start-date="datetime"
+      v-model:end-date="datetime"
+      :timezone="timezone"
+    />
+
   </div>
 </template>
